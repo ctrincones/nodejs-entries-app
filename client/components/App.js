41f4f15a1880dfc83d7b7c571943f54865b15f35
Carlos Rincones <carlostomasrincones@gmail.com>
2017-01-26 16:09:37 -0500

    hot reload configured

--- client/components/App.js
+++ client/components/App.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-  return (
-    <h1>Hello from react</h1>
-  );
+class App extends Component {
+  render(){
+    return (
+      <h1>Hello from react with hot reload </h1>
+    );
+  }
 }
+
+export default App;
